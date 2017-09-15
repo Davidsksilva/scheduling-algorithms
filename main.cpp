1529800d6669ceb2eac47aad040d36c4360d3ede
David <davidsimonkastle@gmail.com>
2017-09-14 22:26:15 -0300

    Update

--- main.cpp
+++ main.cpp
@@ -93,15 +93,68 @@ void FCFS(int processo [][2],int n)
 {
   ordernarProcessosChegada(processo,n);
   float tempo_retorno_medio=0;
+  float tempo_resposta_medio=0;
+  float tempo_espera_medio=0;
   int tempo_acumulado=0;
   for(int i=0;i<n;i++)
   {
-    tempo_retorno_medio=tempo_acumulado+processo[i][1];
+    tempo_retorno_medio+=tempo_acumulado+processo[i][1];
+    tempo_resposta_medio+=tempo_acumulado;
+    tempo_espera_medio+=tempo_acumulado;
     tempo_acumulado+=processo[i][1];
   }
   tempo_retorno_medio/=n;
+  tempo_resposta_medio/=n;
+  tempo_espera_medio/=n;
 
-  cout<<"FCFS "<<std::setprecision(1)<<tempo_retorno_medio<<endl;
+  cout<<"FCFS "<<std::fixed<<std::setprecision(1)<<tempo_retorno_medio<<" "
+  <<tempo_resposta_medio<<" "<<tempo_espera_medio<<endl;
+}
+
+int proximoProcessoSJF(int processo[][2], int n, int t)
+{
+  int id_min_duration=2147483647;
+  for(int i=0;i<n;i++)
+  {
+    if((processo[i][0]<=t) && (processo[i][1]!= 0))
+    {
+      if(id_min_duration==2147483647)
+        id_min_duration=i;
+      else if(processo[i][1]< processo[id_min_duration][1])
+          id_min_duration=i;
+    }
+  }
+  if(id_min_duration==2147483647)
+    return -1;
+  else
+   return id_min_duration;
+}
+void SJF(int processo [][2],int n)
+{
+  ordernarProcessosChegada(processo,n);
+  printProcesses(processo,n);
+
+  float tempo_retorno_medio=0;
+  float tempo_resposta_medio=0;
+  float tempo_espera_medio=0;
+  int tempo_acumulado=0;
+  int id=0;
+  tempo_acumulado=processo[0][0];
+  while((id=proximoProcessoSJF(processo,n,tempo_acumulado)) != -1)
+  {
+    cout<<id<<endl;
+    tempo_retorno_medio+=tempo_acumulado+processo[id][1];
+    tempo_resposta_medio+=tempo_acumulado;
+    tempo_espera_medio+=tempo_acumulado;
+    tempo_acumulado+=processo[id][1];
+    processo[id][1]=0;
+  }
+  tempo_retorno_medio/=n;
+  tempo_resposta_medio/=n;
+  tempo_espera_medio/=n;
+
+  cout<<"SJF "<<std::fixed<<std::setprecision(1)<<tempo_retorno_medio<<" "
+  <<tempo_resposta_medio<<" "<<tempo_espera_medio<<endl;
 }
 
 int main()
@@ -110,9 +163,9 @@ int main()
   n=getLines();
   int processo[n][2];
   getProcessData(processo);
-  printProcesses(processo,n);
   FCFS(processo,n);
-  //
+  SJF(processo,n);
+
 
   return 0;
 }
